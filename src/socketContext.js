fa87f864f6903a1b177440398c5111c3e3e4fd3d
Proust-stack <andrey-hunter@yandex.ru>
2022-11-28 15:15:29 +0400

    done

--- src/socketContext.js
+++ src/socketContext.js
@@ -1,7 +1,7 @@
 import React, { createContext } from "react";
 import { io, Socket } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://tictuctoetask7.herokuapp.com", {
   transports: ["websocket"],
 });
 const SocketContext = createContext(socket);
